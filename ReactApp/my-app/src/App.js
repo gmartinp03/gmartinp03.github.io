7955b8c4b8d92049298df60e6d731c140ad84440
Gabriel Pederson <gmartinp03@outlook.com>
2024-07-23 22:35:40 -0500

    Add text updates to cards, add BAE SW experience card, fix typos

--- ReactApp/my-app/src/App.js
+++ ReactApp/my-app/src/App.js
@@ -10,18 +10,29 @@ function App() {
     {
       id: 1,
       title: "BAE Systems",
-      subtitle: "Technical Intern",
+      subtitle: "Software Engineer Intern",
+      date: "May -> Aug '24",
       image: "./images/BAE.jpg",
       detail:
-        "From the Summer to winter of 2023, I am on Co-op for BAE, supporting the systems team. I primarily use Python, Excel, and DOORS to create test artifacts and perfrom assesment on GPS receiver software/haredware. I also run regression and integration tests to catch new failures as the program progesses.",
+        "I am really grateful to have been a part of the software team. I had the opportunity to develop features for various GPS receivers, upgrade component interfaces, and update IO messages in C/C++. It was a great experience and I learned a lot about the software development process.",
     },
     {
       id: 2,
+      title: "BAE Systems",
+      subtitle: "Systems Engineer Intern",
+      date: "May -> Dec '23",
+      image: "./images/BAE.jpg",
+      detail:
+        "I was on the Systems team, No Test for the Wicked, as a Co-op for BAE. I created test artifacts using Python, Excel, and IBM Doors. In addition, I performed assesment on GPS receiver software/hardware and ran continuous integration tests against incoming software as the program progressed.",
+    },
+    {
+      id: 3,
       title: "MidAmerican Energy",
       subtitle: "Engineering Intern",
+      date: "Apr -> Aug '22",
       image: "./images/MidAmWind.jpg",
       detail:
-        "In the Summer of 2022, I supported renewable generation operations by reviewing project documentation and data mining turbine data for lightning protection system upgrades. I mainly used Excel to track serial numbers, dates, and turbine information for over 3,000 lightning protection systems and to automatically accept/reject reports. I also compiled turbine metadata to create standard reference maps for contractor use.",
+        "For my first internship, I supported Renewable Generation operations by reviewing project documentation and data mining wind turbine data for lightning protection system upgrades. I mainly used Excel to track information for over 3,000 turbines, and I built a system to automatically accept/reject reports. I also compiled turbine metadata to create maps of wind farms for contractors to use.",
     },
   ];
   const project = [
@@ -30,40 +41,47 @@ function App() {
       title: "Sit With Me",
       subtitle: "React, MapLibre, FireStore",
       detail:
-        "Won First Place Overall against 50 ISU students. Developed a social media website on a team of four, aiming to help students struggling with social anxiety and loneliness.",
+        "Won First Place Overall against 50 ISU students. Developed a social media website on a team of four, aiming to help students struggling with social anxiety by making that first question, 'Is that seat open?', a little easier.",
     },
     {
       id: 2,
       title: "CourtUp",
       subtitle: "React, Spring, MySQL",
       detail:
-        "Full stack web application dedicated to finding basketball courts near you. WIP",
+        "Full stack web application dedicated to finding basketball courts near you. Implemented endpoints to perform CRUD operations on various data models and handled web API requests to update the UI based on responses",
     },
     {
       id: 3,
-      title: "CyFly",
-      subtitle: "MATLAB, SolidWorks, xflr5",
+      title: "Honda CB750k4",
+      subtitle: "Project Bike",
       detail:
-        "Researched and designed an aircraft theoretically capable of flight to complete a mission. Built MATLAB scripts to process data from xflr5 and present the aircrafts performance.",
+        "My joy... not yet my pride. Needs a decent amount of work to make it pretty, but I enjoy spending time on it.",
     },
     {
       id: 4,
-      title: "Cyclone Rocketry",
-      subtitle: "Student Org",
+      title: "CyFly",
+      subtitle: "MATLAB, SolidWorks, xflr5",
       detail:
-        "Researched and manufactured parachute ejection systems for the SRAD Spaceport competitions. Created a reefing system to constrict the parachute",
+        "Researched and designed an aircraft theoretically capable of flight to complete a mission. Built MATLAB scripts to process airfoil data from xflr5 and presented the aircraft's performance.",
     },
     {
       id: 5,
-      title: "A380",
-      subtitle: "SolidWorks",
+      title: "Cyclone Rocketry",
+      subtitle: "Student Org",
       detail:
-        "Collaborated on a scale model of an Airbus A380 in SolidWorks. Independantly worked on the fuselage, undercarriage, horizontal stabilizers, and engine/wing connections. Also added a livery to show school pride.",
+        "Researched and manufactured parachute ejection systems for the SRAD Spaceport competitions. Created a reefing system to constrict the parachute at apogee, then fully expand later.",
     },
     {
       id: 6,
+      title: "A380",
+      subtitle: "SolidWorks",
+      detail:
+        "Collaborated on a scale model of an Airbus A380 in SolidWorks. Independently worked on the fuselage, undercarriage, horizontal stabilizers, and engine/wing connections. Also added a livery to show school pride.",
+    },
+    {
+      id: 7,
       title: "Lighter Than Air",
-      subtitle: "Class Project",
+      subtitle: "Freshman Class Project",
       detail:
         "Constructed a balsa wood aircraft based on the 90's game show, Baggage. Worked on a team to ensure the craft was capable of stable flight while meeting a weight limit and budget.",
     },
--- ReactApp/my-app/src/Components/Introduction.js
+++ ReactApp/my-app/src/Components/Introduction.js
@@ -5,11 +5,11 @@ function Intro() {
         <h1 className="title"> I am Gabriel Pederson,</h1>
         <h2 className="subtitle">An Aspiring Engineer</h2>
         <p>
-          I am an engineering student in my junior year at Iowa State
-          University, expecting to graduate in Spring 2025, studying for a
-          Bachelors of Science in Aerospace Engineering with a minor in Computer
+          I am an engineering student in my senior year at Iowa State
+          University, graduating in Spring 2025. I am finishing up my
+          Bachelor of Science in Aerospace Engineering with a minor in Computer
           Science. This website is to highlight experiences and projects in my
-          profesisonal development.
+          professional development, throughout which I discovered a passion for software development.
         </p>
       </section>
     </div>
